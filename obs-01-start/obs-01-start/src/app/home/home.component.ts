2988733764e9367ef7b09e701e3b0fc81d7ac75a
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2022-11-06 17:08:36 -0300

    finalizando a sessao de observables

---
+++ obs-01-start/obs-01-start/src/app/home/home.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription;
+
+  constructor() { }
+
+  ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe(count=>{
+    //   console.log(count);
+    // })
+    const customItervalObservable = Observable.create(observer =>{
+      // observer é o listen e precisamos informa-lo
+      // observer tem os métodos: next, error e complete
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if(count === 5){
+          observer.complete();
+        }
+        if(count > 3){
+          observer.error(new Error('Count is greater 3'));
+        }
+        count++;
+      }, 1000);
+    });
+    
+    this.firstObsSubscription = customItervalObservable
+                                  .pipe(filter(data => {
+                                    return data > 0;
+                                  }), map((data:number) => {
+                                    return 'Round: ' + (data+1);
+                                  })).subscribe(data =>{
+                                    console.log(data);
+                                  }, error => {
+                                    console.log(error);
+                                    alert(error.message);
+                                  }, ()=>{
+                                    console.log('Completed!');
+                                  });
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
+
+}
